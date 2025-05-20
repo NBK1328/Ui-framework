0eef9a70b3324113a506eaa268a5b137420a4d8a
NBK1328 <don.zadorn@yandex.ru>
2025-05-20 23:34:07 +0300

    "Added initialization files for the future project

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language",
+        action="store",
+        default="en",  # Принудительно испанский по умолчанию
+        help="Set browser language (forced to 'es' if not specified)",
+    )
+
+@pytest.fixture(scope="function")
+def browser(request):
+    
+    useLanguage = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': useLanguage})
+   
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(30)
+
+    yield browser
+    browser.quit()
