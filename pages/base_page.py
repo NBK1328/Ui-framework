3db03802bc4e554f41e64e9f0b54fb1a84e58380
NBK1328 <don.zadorn@yandex.ru>
2025-05-21 12:30:37 +0300

    Added POM objects - base_page and main_page. Restructured the main_page test according to POM. Fixed a syntax error in conftest.

---
+++ pages/base_page.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pytest
+
+class BasePage():
+    
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
+
+
+
--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,15 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By. CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link) # Инициализация PageObj, передаем экземпляр драйвера и урла
+    page.open()  # Открытие страницы, из BasePage, наследование в MainPage
+    page.go_to_login_page # Переход на страницу логина, метод MainPage
 
-    time.sleep(20)
 
+
+    
